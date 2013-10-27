4a5f3cad9caa1ef1885db64fb7a251079366f664
Marek Miettinen <marek.miettinen@gmail.com>
2013-10-27 20:53:38 +0200

    Added one controller spec for RegisterCtrl

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,14 +1,28 @@
 describe('videoChat controllers', function() {
+	var mockUsersService;
   beforeEach(module('videoChat.controllers'));
+  beforeEach(function(){
+  	mockUsersService = jasmine.createSpyObj('usersService', ['register']);
+  	module(function($provide){
+  		$provide.value('usersService', mockUsersService);
+  	});
+  	
+  })
  
-  describe('UsersCtrl', function(){
- 
-    it('should foo', inject(function($controller) {
-      	var scope = {},
-        ctrl = $controller('UsersCtrl', { $scope: scope });
- 		
-      	expect(true).toBe(true);
+  describe('RegisterCtrl', function(){
+  	describe('register', function(){
+	    it('should get called', inject(function($controller) {
+	      	var scope = {},
+	        ctrl = $controller('RegisterCtrl', { $scope: scope });
 
-    }));
+	        scope.user = {};
+	        scope.user.name = "Marek";
+	        scope.register();
+	 		
+	      	expect(mockUsersService.register).toHaveBeenCalled();
+
+	    }));
+  	});
+ 
   });
 });
